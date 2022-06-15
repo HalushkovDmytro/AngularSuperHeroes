35d20d61c1f178b332bdf43b2acf9d754aec90f0
Halushkov Dmytro <Halushkovdmitry93@gmail.com>
2022-06-15 20:06:13 +0300

    Update power-ups.component.ts

--- src/app/main-layout/hero-info-page/power-ups/power-ups.component.ts
+++ src/app/main-layout/hero-info-page/power-ups/power-ups.component.ts
@@ -10,19 +10,6 @@ export class PowerUpsComponent {
 
   public powerUpsArray: PowerUp[] = JSON.parse(localStorage["powerUps"]);
 
-  public checkUse(powerName: string): void {
-    const selectedPowerUp = this.powerUpsArray.find((item) => item.title === powerName);
-
-    if (selectedPowerUp!.usesLeft === 1) {
-      const neededIndex = this.powerUpsArray.indexOf(selectedPowerUp!)
-      this.powerUpsArray.splice(neededIndex,1)
-    }
-
-    selectedPowerUp!.usesLeft -= 1;
-    localStorage.setItem("powerUps", JSON.stringify(this.powerUpsArray))
-    this.powerUpsArray.sort((a,b) => b.usesLeft - a.usesLeft)
-  }
-
   public trackBy(index: number, item: any): PowerUp {
     return item
   }
