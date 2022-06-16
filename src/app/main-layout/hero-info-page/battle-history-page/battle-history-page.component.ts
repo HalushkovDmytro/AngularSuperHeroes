572a78ed59ec91ea293931be2b1605a1f31c400f
Halushkov Dmytro <Halushkovdmitry93@gmail.com>
2022-06-16 11:43:03 +0300

    Update battle-history-page.component.ts

--- src/app/main-layout/hero-info-page/battle-history-page/battle-history-page.component.ts
+++ src/app/main-layout/hero-info-page/battle-history-page/battle-history-page.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { BattleInfo, PowerUp } from "../../../interfaces";
 import { HeroInfoService } from "../../../hero-info-view/hero-info-service";
 
 @Component({
   selector: 'app-battle-history-page',
   templateUrl: './battle-history-page.component.html',
-  styleUrls: ['./battle-history-page.component.scss']
+  styleUrls: ['./battle-history-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BattleHistoryPageComponent implements OnInit {
   public someArray!: BattleInfo[]
 
-  constructor(public heroesInfoService: HeroInfoService) { }
+ constructor(
+    public heroesInfoService: HeroInfoService,
+    private _cdr: ChangeDetectorRef
+  ) { }
 
   public ngOnInit(): void {}
 
