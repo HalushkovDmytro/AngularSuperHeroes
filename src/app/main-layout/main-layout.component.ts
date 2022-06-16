822ca2a4c700c6079103a53e7334b323c0e3808b
Halushkov Dmytro <Halushkovdmitry93@gmail.com>
2022-06-16 11:20:55 +0300

    Update main-layout.component.ts

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HeroesConfigService } from "./selection-page/heroes.config.service";
 import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainLayoutComponent implements OnInit {
-
+  public params!: string ;
   constructor(
     private _heroesService: HeroesConfigService,
-    private route: Router
+    private route: Router,
+    private _cdr: ChangeDetectorRef
     ) {}
 
   public ngOnInit(): void {
@@ -19,7 +21,12 @@ export class MainLayoutComponent implements OnInit {
     this._heroesService.initOwnedHeroes();
     this._heroesService.initSelectedHero();
     this._heroesService.initSearchedHeroes();
+    this.getQueryParams();
   }
+  
+    public getQueryParams(): void {
+    this.params = this.route.routerState.snapshot.url
+  }  
 
   public goToSelectionPage(): void {
     this.route.navigate(['main/selection-page'])
@@ -27,5 +34,10 @@ export class MainLayoutComponent implements OnInit {
 
   public goToUserInfo(): void {
     this.route.navigate(['main/users-info'])
+  }
+  
+  public goToBattlePage(): void {
+    this.route.navigate(['main/battle'])
+  
   }
 }
