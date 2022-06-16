56c429d7b960b91544fbb072fc7ed1c71d63a566
Halushkov Dmytro <Halushkovdmitry93@gmail.com>
2022-06-16 11:40:27 +0300

    Update heroes-list.component.ts

--- src/app/main-layout/hero-info-page/heroes-list/heroes-list.component.ts
+++ src/app/main-layout/hero-info-page/heroes-list/heroes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { HeroesConfigService } from "../../selection-page/heroes.config.service";
 import { Router } from "@angular/router";
 import { HeroInfo, PowerUp } from "../../../interfaces";
@@ -6,17 +6,20 @@ import { HeroInfo, PowerUp } from "../../../interfaces";
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
-  styleUrls: ['./heroes-list.component.scss']
+  styleUrls: ['./heroes-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroesListComponent {
   @Input() hero!: HeroInfo
 
   constructor(
     public heroesService: HeroesConfigService,
-    private _router: Router
+    private _router: Router,
+    private _cdr: ChangeDetectorRef
     ) {}
 
   public goToSelectPage(): void {
+    this._cdr.markForCheck()
     this._router.navigate(['main/selection-page']);
     window.scrollTo(0, 0);
   }
